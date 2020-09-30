9fca6aaf0258284326a4f5bf53cfb4954d737291
jokvar <jokubas.varnagiris@gmail.com>
2020-09-30 22:37:02 +0300

    yeet

---
+++ T120B165/Migrations/20200930175354_InitialCreate.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace T120B165.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lecturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(maxLength: 60, nullable: false),
+                    ApiKey = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lecturers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(maxLength: 60, nullable: false),
+                    ApiKey = table.Column<string>(nullable: true),
+                    Vidko = table.Column<string>(maxLength: 5, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(nullable: false),
+                    LecturerId = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Modules_Lecturers_LecturerId",
+                        column: x => x.LecturerId,
+                        principalTable: "Lecturers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Modules_LecturerId",
+                table: "Modules",
+                column: "LecturerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Modules");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Lecturers");
+        }
+    }
+}
---
+++ T120B165/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using T120B165.Data;
+
+namespace T120B165.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (T120B165Context context = new T120B165Context(serviceProvider.GetRequiredService<DbContextOptions<T120B165Context>>()))
+            {
+                //Check if objects exist
+                if (context.Modules.Any() || context.Students.Any() || context.Lecturers.Any())
+                {
+                    return; //DB has been seeded
+                }
+                context.Lecturers.AddRange(
+                    new Lecturer
+                    {
+                        FirstName = "Lecturer",
+                        LastName = "Lecturerhausen",
+                        Username = "lecturer1",
+                        Password = "password1"
+                    },
+                    new Lecturer
+                    {
+                        FirstName = "John",
+                        LastName = "Lecturerstein",
+                        Username = "lecturer2",
+                        Password = "password2"
+                    }
+                );
+                context.Students.AddRange(
+                    new Student
+                    {
+                        FirstName = "David",
+                        LastName = "Yanusevich",
+                        Username = "dovydovy",
+                        Password = "dovydovy2"
+                    },
+                    new Student
+                    {
+                        FirstName = "Calvin",
+                        LastName = "Berkhousen",
+                        Username = "3picSexyCalvin",
+                        Password = "calvin123"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
